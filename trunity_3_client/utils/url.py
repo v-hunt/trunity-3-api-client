2821e9f4d540a99fabcbd0a33be74f46804da189
VicHunting <VicHunting@yandex.ua>
2017-07-23 20:59:28 +0300

    Add some fixes to url utils file

--- trunity_3_client/utils/url.py
+++ trunity_3_client/utils/url.py
@@ -1,11 +1,13 @@
 from urllib.parse import urljoin
 
+API_ROOT = 'http://trunity.mkechinov.ru/api/v3/'  # STAGE environment
+# API_ROOT = 'http://trunity.org/api/v3/'         # PROD environment
 URL_DETAIL_PREFIX = '{id}'
 
 
 class Url(object):
 
-    def __init__(self, root: str):
+    def __init__(self, root: str) -> None:
         self._root = root + '/' if not root.endswith('/') else root
         self._tail = None
         self._detail_prefix = URL_DETAIL_PREFIX
